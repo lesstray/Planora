0f7e086cfd85415efb93b809d0f6271858ce27fd
lesstray <dindiberyad@mail.ru>
2025-06-10 01:35:07 +0300

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# Ожидаем доступность БД
 echo "Waiting for PostgreSQL..."
 
 until PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c '\q' 2>/dev/null; do
@@ -9,9 +8,9 @@ until PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c '\q' 2
   sleep 1
 done
 
-echo "PostgreSQL is up - running migrations"
+echo "PostgreSQL is up - applying migrations..."
 
-# Миграции и запуск
-python planora/manage.py makemigrations
-python planora/manage.py migrate
-python planora/manage.py runserver 0.0.0.0:8000
+python planora/manage.py migrate --noinput
+python planora/manage.py collectstatic --noinput || true
+
+exec python planora/manage.py runserver 0.0.0.0:8000
