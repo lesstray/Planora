7eb3ad96f43625e8ff0a3c03e7c61232d48c330b
lesstray <dindiberyad@mail.ru>
2025-06-09 12:55:57 +0300

    some fixes :) \_(^-^)_/

--- planora/planora/settings.py
+++ planora/planora/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'planora',
         'USER': 'postgres',
-        'PASSWORD': 'LinaSniperSF',
+        'PASSWORD': 'Frest4920310',
         'HOST': 'localhost',
         'PORT': '5432'
     }
