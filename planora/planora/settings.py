f60676737a716746e61c7869477580fc134578a6
hokinhim <hokinhimGitHub@gmail.com>
2025-06-10 11:21:27 +0300

    Fix CI/CD

--- planora/planora/settings.py
+++ planora/planora/settings.py
@@ -149,7 +149,7 @@ DATABASES = {
 # Настройка электронной почты
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = 465
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
